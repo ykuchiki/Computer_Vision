6e91155e57811f04d26a511c3d52d4baf3039ec5
Kuchiki <ykuchiki0827@gmail.com>
2023-11-23 18:25:10 +0900

    8

--- 8/main.cpp
+++ 8/main.cpp
@@ -45,9 +45,7 @@ void test_siftmatch(Mat & img1, Mat& img2) {
 	}
 	// draw result
 	drawMatches(img1, keypoints1, img2, keypoints2, good_matches, rgb);
-	//imshow("match", rgb);
     imwrite("level2.jpg", rgb);
-    //waitKey(0);
 
 
 }
@@ -72,6 +70,7 @@ void level3(Mat & img1, Mat& img2) {
 	descriptor->compute(img1, keypoints1, descriptor1);
 	descriptor->compute(img2, keypoints2, descriptor2);
 
+
 	// matching descriptor
 	BFMatcher matcher;
 	vector<DMatch> matches;
@@ -92,21 +91,20 @@ void level3(Mat & img1, Mat& img2) {
     for (size_t i = 0; i < good_matches.size(); i++) {
     points1.push_back(keypoints1[good_matches[i].queryIdx].pt);
     points2.push_back(keypoints2[good_matches[i].trainIdx].pt);
-}
+	}
 
     // 射影行列（ホモグラフィ）を計算
     Mat H = cv::findHomography(points2, points1, RANSAC);
 
-     // 画像を変形（warp）
-    // warp img2
+    // 画像を変形（warp）
     Mat warpedImg2;
     warpPerspective(img2, warpedImg2, H, cv::Size(img1.cols + img2.cols, img1.rows));
     imwrite("test.jpg", warpedImg2);
 
-    // Create a new image to hold the stitched result
+    // 結果保存用
     Mat result(cv::Size(img1.cols + img2.cols*2, img1.rows*2), img1.type());
 
-    // Copy img1 into the new image
+    // imgを結果にコピー
     img1.copyTo(result(cv::Rect(0, 0, img1.cols, img1.rows)));
 
     // img1の右端の座標を準備
@@ -119,26 +117,35 @@ void level3(Mat & img1, Mat& img2) {
     // 座標を変換
     perspectiveTransform(srcPoints, dstPoints, H);
 
-    // 変換された点を取得
-    Point2f transformed_pt = dstPoints[0];
+    warpPerspective(img2, warpedImg2, H, cv::Size(img1.cols + img2.cols, img1.rows));
 
-    // オフセットを計算
-    int x_offset = static_cast<int>(transformed_pt.x - img1.cols);
+	// 重なり合う領域の幅を50ピクセルと仮定
+    int overlapWidth = 50;
 
-     // Determine the position to copy warped img2
-    warpedImg2.copyTo(result(cv::Rect(x_offset, 0, warpedImg2.cols, warpedImg2.rows)));
+	// ブレンド処理
+    for (int x = img1.cols - overlapWidth; x < img1.cols; x++) {
+        int blendWidth = img1.cols - x;
+        for (int y = 0; y < img1.rows; y++) {
+            double alpha = static_cast<double>(blendWidth) / overlapWidth;
+            Vec3b pixel1 = img1.at<Vec3b>(y, x);
+            Vec3b pixel2 = warpedImg2.at<Vec3b>(y, x);
 
-    // 2枚目の画像を結果にコピー
-    //Mat half(result, cv::Rect(0, 0, img2.cols, img2.rows));
-    //img2.copyTo(half);
+            // ピクセル値をブレンド
+            Vec3b blendedPixel;
+            for (int k = 0; k < 3; k++) {
+                blendedPixel[k] = static_cast<uchar>(pixel1[k] * alpha + pixel2[k] * (1 - alpha));
+            }
+            warpedImg2.at<Vec3b>(y, x) = blendedPixel;
+        }
+    }
 
-    // 結果を表示
-    //imshow("Result", result);
-    imwrite("level3.jpg", result);
-    //waitKey(0);
-
-
+    // スティッチング結果を保存
+    img1.copyTo(warpedImg2(cv::Rect(0, 0, img1.cols, img1.rows)));
+    imwrite("level3.jpg", warpedImg2);
 }
+
+
+
 
 int main(){
     // 画像の読み込み
@@ -153,13 +160,11 @@ int main(){
 	test_siftmatch(img1, img2);
 
     //level 3
-    float s = 0.7, theta = 0.3;
+    float s = 0.7, theta = 0.2;
 	Mat A = (Mat_<double>(2, 3) << 
 		 cos(theta) * s, sin(theta) * s,  30,
 		-sin(theta) * s, cos(theta) * s, 140);
 	warpAffine(img2, img2, A, img1.size());
-	//imshow("img2", img2);
-    //waitKey(0);
     imwrite("level3-img2.jpg", img2);
 
     level3(img1,img2);
